04438b66c3be9111ffad21050fa9496a06c1f9c0
spyfer2-ux <spyfer2@gmail.com>
2026-05-12 15:57:13 -0300

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// ATENÇÃO: troque "candeia-franqueado" pelo nome exato do seu repositório no GitHub
+export default defineConfig({
+  plugins: [react()],
+  base: "/candeia-franqueado/",
+});
