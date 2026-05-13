95378bd53aeeb9166b8e562e1c29715eb20ad2d5
spyfer2-ux <spyfer2@gmail.com>
2026-05-13 15:45:45 -0300

    Adiciona configuração do Firebase

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBHk_pJO_-xvIZ3gsiSNNaq7NfHsW-VPZI",
+  authDomain: "candeia-jr.firebaseapp.com",
+  projectId: "candeia-jr",
+  storageBucket: "candeia-jr.firebasestorage.app",
+  messagingSenderId: "825450097768",
+  appId: "1:825450097768:web:da2bc771011825b68c1fdd"
+}
+
+const app = initializeApp(firebaseConfig)
+export const auth = getAuth(app)
+export const db = getFirestore(app)
+export const googleProvider = new GoogleAuthProvider()
+
+export const loginGoogle = () => signInWithPopup(auth, googleProvider)
+export const logout = () => signOut(auth)
